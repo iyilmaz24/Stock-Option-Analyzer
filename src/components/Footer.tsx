638dc4d8b2cc242342818b22cb67001939fe19d3
iyilmaz24 <iy.usa@outlook.com>
2025-10-04 12:52:35 -0700

    Add Header, Footer components and use them in layout.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-gray-950 py-4 px-4 sm:px-6 border-t border-gray-700">
+      <div className="container mx-auto text-center text-gray-500 text-sm">
+        <p>&copy; 2025 Stock Option Analyzer. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-gray-950 backdrop-blur-sm border-b border-gray-700 shadow-lg flex items-center justify-between px-14 sm:px-18">
+      <div className="flex items-center gap-4 px-4 sm:px-6 h-20">
+        <Image
+          src="/bull-bear-logo.png"
+          alt="Bull and Bear logo"
+          width={96}
+          height={80}
+        />
+        <h1 className="text-xl sm:text-2xl font-extrabold tracking-tight text-gray-200">
+          Stock Option Analyzer
+        </h1>
+      </div>
+      <div className="flex items-center gap-4 px-4 sm:px-6 h-20">
+        <Link
+          href="/"
+          className="text-gray-200 font-bold hover:text-white transition-colors"
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="text-gray-200 font-bold hover:text-white transition-colors"
+        >
+          About
+        </Link>
+      </div>
+    </header>
+  );
+}
