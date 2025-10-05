5887d10d1316a417099a08ea031f8df4c1010a2a
iyilmaz24 <iy.usa@outlook.com>
2025-10-05 16:29:49 -0700

    Use ticker from URL's searchParams for /analyze page, handle error paths

--- src/app/analyze/page.tsx
+++ src/app/analyze/page.tsx
@@ -1,20 +1,50 @@
-import AnalyzeInput from "@/components/AnalyzeInput";
 import { marketDataService } from "../api/services/marketDataService";
+import AnalyzeInput from "@/components/AnalyzeInput";
 import PriceData from "@/components/data/PriceData";
 import QuoteData from "@/components/data/QuoteData";
 import Headlines from "@/components/data/Headlines";
 
-export default function AnalyzePage() {
-  const marketDataJson = marketDataService.getMarketData("AAPL");
+export default async function AnalyzePage({
+  searchParams,
+}: {
+  searchParams: { ticker?: string };
+}) {
+  const ticker = searchParams.ticker || "";
+  let marketDataJson = null;
 
+  if (ticker) {
+    marketDataJson = await marketDataService.getMarketData(ticker);
+  }
   return (
     <div className="bg-gray-900 text-white min-h-screen flex flex-col">
       <main className="flex-grow flex flex-col items-center p-16 text-center">
         <AnalyzeInput />
 
-        <PriceData priceData={marketDataJson.priceData} />
-        <QuoteData quoteData={marketDataJson.quoteData} />
-        <Headlines headlines={marketDataJson.headlines} />
+        {ticker ? (
+          marketDataJson ? (
+            <>
+              <PriceData priceData={marketDataJson.priceData} />
+              <QuoteData quoteData={marketDataJson.quoteData} />
+              <Headlines headlines={marketDataJson.headlines} />
+            </>
+          ) : (
+            <p className="mt-24 p-10 rounded-md bg-red-900/50 border border-red-700 text-red-300">
+              No data found for &quot;{ticker}&quot;
+              <br /> <br /> Possible reasons include:
+              <ul className="list-disc list-inside text-left">
+                <li>Invalid ticker symbol</li>
+                <li>Connection issues</li>
+                <li>Data source issue</li>
+              </ul>
+            </p>
+          )
+        ) : (
+          <p className="mt-24 p-10 rounded-md bg-yellow-900/50 border border-yellow-700 text-yellow-300">
+            Please enter a ticker symbol above.
+            <br />
+            e.g. AAPL, MSFT, GOOGL
+          </p>
+        )}
       </main>
     </div>
   );
