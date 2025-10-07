2a7c3ebd152b23674edf94b90d0fa2f6102a67f4
iyilmaz24 <iy.usa@outlook.com>
2025-10-06 21:29:03 -0700

    Handle NaN values for dividend yield data (stock not paying dividends)

--- src/components/data/QuoteData.tsx
+++ src/components/data/QuoteData.tsx
@@ -9,7 +9,10 @@ export default function QuoteData({ quoteData }: { quoteData: QuoteDataT }) {
       <p>P/E: {quoteData.peRatio}</p>
       <p>Trailing P/E: {quoteData.trailingPE}</p>
       <p>Forward P/E: {quoteData.forwardPE}</p>
-      <p>Dividend Yield: {quoteData.dividendYield}</p>
+      <p>
+        Dividend Yield:{" "}
+        {quoteData.dividendYield ? `${quoteData.dividendYield}` : "N/A"}
+      </p>
       <p>Expected Surprise: {quoteData.expectedSurprise}</p>
       <p>Earnings Date: {quoteData.earningDate}</p>
       <p>Beta: {quoteData.beta}</p>
